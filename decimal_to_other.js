436eb5fe56db2db45dd50ee49aac4bcec9e39965
Roshan Raj <roshan139154@gmail.com>
2020-08-23 11:59:39 +0530

    Wrote comments

--- decimal_to_other.js
+++ decimal_to_other.js
@@ -12,7 +12,7 @@ dec_to_bho = function (n, base) {
             break;
         case 'O':
             return parseInt(n, 10).toString(8);
-            break;
+            break;  
         default:
             return ("Wrong input.........");
     }
@@ -20,8 +20,8 @@ dec_to_bho = function (n, base) {
 
 let decimal_to_binary = (number) => {
 
-    let binary = ""; //Store the binary number here
-    let temp = number;
+    let binary = ""; // Variable to store the binary number here
+    let temp = number; // Temporary variable to store the input
 
     while (temp > 0) {
         if (temp % 2 == 0) {
@@ -31,10 +31,9 @@ let decimal_to_binary = (number) => {
             binary = "1" + binary;
         }
 
-        temp = Math.floor(temp / 2);
+        temp = Math.floor(temp / 2); // To round off the number, Temp is the quotient
     }
-
     return binary;
 }
 
-console.log(decimal_to_binary(15))
+console.log(decimal_to_binary(75))
